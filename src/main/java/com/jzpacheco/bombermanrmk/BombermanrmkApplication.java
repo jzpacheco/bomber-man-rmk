555090193fc7409179c1227857127d4c8f99085d
jzpacheco <jeffersonpacheco@hotmail.com>
2024-01-06 22:39:46 -0300

    initial commit

---
+++ src/main/java/com/jzpacheco/bombermanrmk/BombermanrmkApplication.java
@@ -0,0 +1,13 @@
+package com.jzpacheco.bombermanrmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombermanrmkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BombermanrmkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jzpacheco/bombermanrmk/BombermanrmkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzpacheco.bombermanrmk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombermanrmkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
