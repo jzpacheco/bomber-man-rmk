2c739a74b46957c348075bb63367c45e5aafcc13
jzpacheco <jeffersonpacheco@hotmail.com>
2024-01-10 23:37:29 -0300

    feat: created fixed obsctacle and improved map layout

--- src/main/resources/static/src/app.js
+++ src/main/resources/static/src/app.js
@@ -1,4 +1,4 @@
-const mapSize = {x: 40, y:20}
+const mapSize = {x: 21, y:41}
 
 
 const mapContainer = document.getElementById('map')
@@ -7,20 +7,27 @@ const player = document.createElement('div')
 player.id = 'player'
 mapContainer.appendChild(player)
 
-playerPosition = {x:1,y:1}
+playerPosition = {x:0,y:0}
 
 map = []
 function createMap(){
     for (let i = 0; i < mapSize.x; i++) {
         map[i] = []
         for (let j = 0; j < mapSize.y; j++) {
-             map[i][j] = Math.floor(Math.random() * 2);
+            if (i % 2 != 0 && j % 2 != 0){
+                map[i][j] = 2
+            } else if ((i <= 1 && j <= 1) || (i <= 1 && j >= mapSize.y - 2) || (i >= mapSize.x - 2 && j <= 1) || (i >= mapSize.x - 2 && j >= mapSize.y - 2)) {
+                map[i][j] = 0
+              } else {
+                map[i][j] = Math.floor(Math.random() * 2);
+            }
         }
         
     }
 }
+
+
 createMap()
-console.log(map)
 
 function updatePlayerPosition(){
     player.style.transform = `translate(${playerPosition.x *27}px, ${playerPosition.y *27}px)`
@@ -34,8 +41,10 @@ function renderMap() {
 
             if (cellType === 0){
                 cell.classList.add('empty')
-            } else {
+            } else if (cellType === 1) {
                 cell.classList.add('wall')
+            } else {
+                cell.classList.add('fixed-obstacle')
             }
 
             mapContainer.appendChild(cell);
