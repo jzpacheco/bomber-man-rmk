d6ef792e9b1509a1392449a37287f1f63f23d264
jzpacheco <jeffersonpacheco@hotmail.com>
2024-02-08 23:49:21 -0300

    created game service and repository

--- src/main/java/com/jzpacheco/bombermanrmk/repositories/GameRepository.java
+++ src/main/java/com/jzpacheco/bombermanrmk/repositories/GameRepository.java
@@ -1,4 +1,25 @@
 package com.jzpacheco.bombermanrmk.repositories;
 
+import com.jzpacheco.bombermanrmk.model.Game;
+import com.jzpacheco.bombermanrmk.model.Player;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class GameRepository {
+    private HashMap<String, Game> games = new HashMap<>();
+
+    public Game findById(Integer id){
+        return  games.get(id);
+    }
+
+    public Game postPlayer(Game game){
+        games.put(game.getPassword(), game);
+
+        return games.get(game.getPassword());
+    }
+
+    public List<Game> findAll(){
+        return games.values().stream().toList();
+    }
 }
---
+++ src/main/java/com/jzpacheco/bombermanrmk/services/GameService.java
@@ -0,0 +1,25 @@
+package com.jzpacheco.bombermanrmk.services;
+
+import com.jzpacheco.bombermanrmk.model.Game;
+import com.jzpacheco.bombermanrmk.model.Player;
+import com.jzpacheco.bombermanrmk.repositories.GameRepository;
+import com.jzpacheco.bombermanrmk.repositories.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class GameService {
+    private GameRepository repository;
+
+    public Game findById(Integer id){
+        return  repository.findById(id);
+    }
+
+    public List<Game> findAll(){
+        return repository.findAll();
+    }
+    public Game create(Game game){
+       return repository.postPlayer(game);
+    }
+
+}
