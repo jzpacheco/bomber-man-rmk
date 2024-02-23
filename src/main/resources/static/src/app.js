505a7feebbb3b88131403ff753d599852c364426
jzpacheco <jeffersonpacheco@hotmail.com>
2024-02-22 23:46:34 -0300

    .

--- src/main/resources/static/src/app.js
+++ src/main/resources/static/src/app.js
@@ -15,7 +15,7 @@ let player;
 let connectionType;
 playerDiv.id = 'player'
 mapContainer.appendChild(playerDiv)
-
+//TO-DO: Organize project
 
 const stompClient = new StompJs.Client({
     brokerURL: 'ws://localhost:8080/bomberman-ws'
