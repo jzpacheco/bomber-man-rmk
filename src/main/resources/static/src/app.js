8f501b2f4c07b0e6f1b0fd8c108580a0d9156c60
jzpacheco <jeffersonpacheco@hotmail.com>
2024-02-25 23:49:55 -0300

    .

--- src/main/resources/static/src/app.js
+++ src/main/resources/static/src/app.js
@@ -15,7 +15,7 @@ let player;
 let connectionType;
 playerDiv.id = 'player'
 mapContainer.appendChild(playerDiv)
-//TO-DO: Organize , test player insert and dockerize
+//TO-DO: Organize , test player insert and dockerize and get tha shit done
 
 const stompClient = new StompJs.Client({
     brokerURL: 'ws://localhost:8080/bomberman-ws'
